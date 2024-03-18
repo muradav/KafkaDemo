5a18195cb80469f70a4d75b458d1e9c4b3d10cb8
Murad <muradav@code.edu.az>
2024-03-18 14:54:00 +0400

    Add project files.

---
+++ KafkaDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using KafkaDemo;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, collection) =>
+    {
+        collection.AddHostedService<KafkaConsumerHostedService>(); 
+        collection.AddHostedService<KafkaProducerHostedService>();
+    });
+
+CreateHostBuilder(args).Build().Run();
